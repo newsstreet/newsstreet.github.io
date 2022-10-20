e2202daf4cf3de7e34ad0f8d4962fbb8a17a8208
parvind.sharma <parvind.sharma@investisdigital.com>
2022-10-20 13:34:07 +0530

    source code

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>ReactJS app deployed on githubpages</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
